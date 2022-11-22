48afca9ec88e5fd6cff44f79e162b5af30d33964
Mariaclara0310 <ericapolo10@hotmail.com>
2022-11-22 15:11:51 -0500

    organizando vista

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-router.get('/', function(req, res, next) {
-  res.render('catalogo', { title: 'Bienvenid@ a tu tienda Online Invitado' });
+
+router.get('/catalogo', function(req, res, next) {
+  res.render('catalogo');
 });
 
 module.exports = router;
